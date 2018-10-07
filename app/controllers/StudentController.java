403fef79f56d3c2893c83e405dc4757ef12b31a8
Dennis Grabowski <dennisgrabowski@gmx.de>
2018-10-07 15:30:10 +0200

    app: add existing sources
    Small app designed to show how views work in the Play framework for a
    presentation in an university project.

---
+++ app/controllers/StudentController.java
@@ -0,0 +1,55 @@
+package controllers;
+
+import play.mvc.Controller;
+import play.mvc.Result;
+import play.data.*;
+import services.Counter;
+
+import javax.inject.Inject;
+import javax.inject.Singleton;
+
+import models.*;
+
+import views.html.*;
+import java.util.*;
+
+/**
+ * This controller demonstrates how to use dependency injection to
+ * bind a component into a controller class. The class contains an
+ * action that shows an incrementing count to users. The {@link Counter}
+ * object is injected by the Guice dependency injection system.
+ */
+@Singleton
+public class StudentController extends Controller {
+
+    private final ArrayList<Student> studentList;
+    private FormFactory formFactory;
+    private Form<Student> studentForm;
+
+    @Inject
+    public StudentController(FormFactory formFactory) {
+        this.studentList = new ArrayList<>();
+        studentForm = formFactory.form(Student.class);
+        this.formFactory = formFactory;
+    }
+
+    /**
+     * An action that responds with the {@link Counter}'s current
+     * count. The result is plain text. This action is mapped to
+     * <code>GET</code> requests with a path of <code>/count</code>
+     * requests by an entry in the <code>routes</code> config file.
+     */
+    public Result showStudents() {
+        return ok(studentsView.render(studentList));
+    }
+
+    public Result addStudent() {
+        return ok(studentsAdd.render(studentForm));
+    }
+
+    public Result add() {
+        Student s = studentForm.bindFromRequest().get();
+        studentList.add(s);
+        return redirect(routes.StudentController.showStudents());
+    }
+}
---
+++ app/models/Student.java
@@ -0,0 +1,26 @@
+package models;
+
+public class Student {
+    public String firstName;
+    public String lastName;
+    public String mail;
+    public int matNr;
+
+    public Student() {
+        firstName = "";
+        lastName = "";
+        mail = "";
+        matNr = 0;
+    }
+
+    public Student(String firstName, String lastName, String mail, int matNr) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.mail = mail;
+        this.matNr = matNr;
+    }
+
+    public String fullName() {
+        return firstName + " " + lastName;
+    }
+}
